2bcd1c8e4929864681515a2039b18377cb4666cd
Rajeshwar Kurapaty <rkurapat@codeaurora.org>
2020-09-17 13:35:01 +0530

    rootdir: Set Netflix property for scuba
    Set "ro.netflix.bsp_rev" for scuba with certified value.
    
    Change-Id: Ia296e4c88fce802133a38c31bd7d48507d1398a2

--- rootdir/etc/init.qcom.early_boot.sh
+++ rootdir/etc/init.qcom.early_boot.sh
@@ -378,7 +378,7 @@ case "$target" in
                 setprop vendor.media.target.version 2
                 setprop vendor.gralloc.disable_ubwc 1
                 setprop vendor.display.enhance_idle_time 1
-                setprop vendor.netflix.bsp_rev ""
+                setprop vendor.netflix.bsp_rev "Q4125-32106-1"
                 # 196609 is decimal for 0x30001 to report version 3.1
                 setprop vendor.opengles.version     196609
                 sku_ver=`cat /sys/devices/platform/soc/5a00000.qcom,vidc1/sku_version` 2> /dev/null
@@ -391,7 +391,7 @@ case "$target" in
                 setprop vendor.fastrpc.disable.cdsprpcd.daemon 1
                 setprop vendor.gralloc.disable_ubwc 1
                 setprop vendor.display.enhance_idle_time 1
-                setprop vendor.netflix.bsp_rev ""
+                setprop vendor.netflix.bsp_rev "Q4125-32106-1"
                 ;;
             *)
                 # default case is for bengal
